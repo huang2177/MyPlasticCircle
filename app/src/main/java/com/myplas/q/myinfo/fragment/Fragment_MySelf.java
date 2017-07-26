ff96ca00df96f1945bff01bc62fe8eb179c443b2
PC-20170512QTFZ\Administrator <15378412400@163.com>
2017-07-26 09:14:26 +0800

    commit3333

--- app/src/main/java/com/myplas/q/myinfo/fragment/Fragment_MySelf.java
+++ app/src/main/java/com/myplas/q/myinfo/fragment/Fragment_MySelf.java
@@ -58,7 +58,6 @@ import java.util.Map;
  * 邮箱：15378412400@163.com
  * 时间：2017/3/17 14:45
  */
-//3333333
 public class Fragment_MySelf extends Fragment implements View.OnClickListener, ResultCallBack, DialogShowUtils.DialogShowInterface, DownloadApk.InstallInterface {
     private View view;
     private MyZone myZone;
